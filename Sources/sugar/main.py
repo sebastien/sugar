99896c1a634749484f9588d4876685d224733dfc
Sebastien Pierre <sebastien@type-z.org>
2008-12-16 09:46:56 -0500

    Added module merging tests

--- Sources/sugar/main.py
+++ Sources/sugar/main.py
@@ -8,23 +8,24 @@ import grammar
 class Command(BaseCommand):
 
 	def setupEnvironment( self ):
-			self.environment.resolver = AbstractResolver()
-			python_plugin       = self.environment.loadLanguage("python")
-			javascript_plugin   = self.environment.loadLanguage("javascript")
-			actionscript_plugin = self.environment.loadLanguage("actionscript")
-			pnuts_plugin        = self.environment.loadLanguage("pnuts")
-			python_plugin.addRecognizedExtension("spy")
-			javascript_plugin.addRecognizedExtension("sjs")
-			actionscript_plugin.addRecognizedExtension("sas")
-			pnuts_plugin.addRecognizedExtension("spnuts")
-			pnuts_plugin.addRecognizedExtension("spnut")
-			python_plugin.reader = grammar.Parser
-			python_reader = python_plugin.reader()
-			python_reader.environment = self.environment
-			# FIXME: This is temporary, until we use the proper reader interface
-			python_reader._program = self.environment.getProgram()
-			# FIXME: This should be done by the Parser itself
-			self.environment.addParser(python_reader, "sg spy sjs sjava spnuts sas".split())
+		self.environment.resolver = AbstractResolver()
+		python_plugin       = self.environment.loadLanguage("python")
+		javascript_plugin   = self.environment.loadLanguage("javascript")
+		actionscript_plugin = self.environment.loadLanguage("actionscript")
+		pnuts_plugin        = self.environment.loadLanguage("pnuts")
+		python_plugin.addRecognizedExtension("spy")
+		javascript_plugin.addRecognizedExtension("sjs")
+		actionscript_plugin.addRecognizedExtension("sas")
+		pnuts_plugin.addRecognizedExtension("spnuts")
+		pnuts_plugin.addRecognizedExtension("spnut")
+		# WTF ?? I don't get this code
+		python_plugin.reader = grammar.Parser
+		python_reader = python_plugin.reader()
+		python_reader.environment = self.environment
+		# FIXME: This is temporary, until we use the proper reader interface
+		python_reader._program = self.environment.getProgram()
+		# FIXME: This should be done by the Parser itself
+		self.environment.addParser(python_reader, "sg spy sjs sjava spnuts sas".split())
 
 def sourceFileToJavaScript( path, moduleName=None ):
 	command = Command()
