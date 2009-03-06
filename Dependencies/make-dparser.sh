f23825b11c2e84d5a2e95c46773b6a5a8a2eeaa9
Sebastien Pierre <sebastien@type-z.org>
2009-03-06 17:58:47 -0500

    Updated make-dparser script

--- Dependencies/make-dparser.sh
+++ Dependencies/make-dparser.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # NOTE: This script must be run from the Sugar/Dependencies directory
 DPARSER=d-1.15-patched
+if [ $PYTHONPATH ]; then
+	PPATH=$PYTHONPATH
+else
+	PPATH=`python -c "import sys;print ':'.join(filter(lambda x:x and x.find('.egg') == -1,sys.path))"`
+fi
+
 mkdir dparser
 pushd $DPARSER
 	make
@@ -9,12 +15,13 @@ pushd $DPARSER
 	cp `find build -name "dparser.py"` ../../dparser
 	cp `find build -name "dparser_swigc.so"` ../../dparser
 popd
+# FIXME: In some cases, there is no PYTHONPATH
 echo ===========================================================
 echo NOTE: Please copy '../../dparser/*' to one of this location
 echo ===========================================================
-echo $PYTHONPATH | sed 's|:|\n|g' | sort
+echo $PPATH | sed 's|:|\n|g' | sort
 echo ===========================================================
 echo For example:
-echo $ cp '../../dparser/*' `echo $PYTHONPATH|cut -d':' -f1`
+echo $ cp 'dparser/*' `echo $PPATH|cut -d':' -f1`
 echo ===========================================================
 # EOF
